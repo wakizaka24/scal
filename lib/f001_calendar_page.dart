b412c447a5ee80aea7bc4745562ea97bbabc4c9c
Ryota Wakizaka <wakizaka24@gmail.com>
2023-06-08 23:11:56 +0900

    カレンダーを実装する

--- lib/f001_calendar_page.dart
+++ lib/f001_calendar_page.dart
@@ -3,11 +3,46 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'f002_end_drawer.dart';
 
+const borderColor = Color(0xCCDED2BF);
+
 class CalendarPage extends HookConsumerWidget {
   const CalendarPage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    // Widgetの一番上で取得可能な項目
+    // アンセーフエリア上の高さ
+    double unSafeAreaTopHeight = MediaQuery.of(context).padding.top;
+    // アンセーフエリア下の高さ
+    double unSafeAreaBottomHeight = 0;
+
+    // 画面の幅
+    double deviceWidth = MediaQuery.of(context).size.width;
+    // 画面の高さ
+    double deviceHeight = MediaQuery.of(context).size.height;
+    // アプリバーの高さ
+    double appBarHeight = AppBar().preferredSize.height;
+    // 週部分の高さ
+    double weekPartHeight = 32;
+    // イベント一覧のアスペクト比
+    double eventListAspectRate = 1.4;
+    // 月部分の高さ
+    double monthPartHeight = deviceHeight - deviceWidth
+        / eventListAspectRate - appBarHeight - weekPartHeight
+        - unSafeAreaTopHeight - unSafeAreaBottomHeight;
+    // 週部分の列数
+    int weekPartColumnNum = 7;
+    // 週部分の幅
+    double weekPartWidth = deviceWidth / weekPartColumnNum;
+    // 週部分のアスペクト比
+    double weekPartAspectRate = weekPartWidth / weekPartHeight;
+    // 日部分の行数
+    int dayPartRowNum = 6;
+    // 日部分の高さ
+    double dayPartHeight = monthPartHeight / dayPartRowNum;
+    // 日部分のアスペクト比
+    double dayPartAspectRate = weekPartWidth / dayPartHeight;
+
     return Scaffold(
         appBar: AppBar(
           title: const Row(
@@ -19,18 +54,119 @@ class CalendarPage extends HookConsumerWidget {
         ),
         endDrawer: const EndDrawer(),
         body: SafeArea(
+          bottom: false,
           child: Column(
             children: [
-              Container(
-                height: 52,
-                color: const Color(0xCCDED2BF),
-                padding: const EdgeInsets.symmetric(
-                    horizontal: 16, vertical: 16),
+              Expanded(
+                  child: PageView(
+                      children: [
+                        for (int i=0; i < 3; i++) ... {
+                          MonthPart(
+                              weekPartColumnNum: weekPartColumnNum,
+                              weekPartAspectRate: weekPartAspectRate,
+                              weekPartHeight: weekPartHeight,
+                              dayPartRowNum: dayPartRowNum,
+                              dayPartAspectRate: dayPartAspectRate,
+                              dayPartHeight: dayPartHeight
+                          ),
+                        }
+                      ]
+                  )
+              ),
+              AspectRatio(
+                  aspectRatio: 1.4,
+                  child: Container(color: const Color(0xCCDED2BF))
               )
-
             ],
           ),
         )
+    );
+  }
+}
+
+class MonthPart extends HookConsumerWidget {
+  const MonthPart({
+    super.key,
+    required this.weekPartColumnNum,
+    required this.weekPartAspectRate,
+    required this.weekPartHeight,
+    required this.dayPartRowNum,
+    required this.dayPartAspectRate,
+    required this.dayPartHeight,
+  });
+
+  final int weekPartColumnNum;
+  final double weekPartAspectRate;
+  final double weekPartHeight;
+  final int dayPartRowNum;
+  final double dayPartAspectRate;
+  final double dayPartHeight;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Column(children: [
+      GridView.count(
+        shrinkWrap: true,
+        // physics: const NeverScrollableScrollPhysics(),
+        padding: EdgeInsets.zero, // セーフエリア下非表示を無効にすると下に余白ができる
+        crossAxisCount: weekPartColumnNum, // 列の数
+        childAspectRatio: weekPartAspectRate, // アスペクト比
+        children: [
+          for (int i=0; i < weekPartColumnNum; i++) ... {
+            WeekPart(height: weekPartHeight),
+          }
+        ],
+      ),
+      GridView.count(
+        shrinkWrap: true,
+        // physics: const NeverScrollableScrollPhysics(),
+        padding: EdgeInsets.zero, // セーフエリア下非表示を無効にすると下に余白ができる
+        crossAxisCount: weekPartColumnNum, // 列の数
+        childAspectRatio: dayPartAspectRate, // アスペクト比
+        children: [
+          for (int i=0; i < weekPartColumnNum * dayPartRowNum; i++) ... {
+            DayPart(height: dayPartHeight),
+          }
+        ],
+      )
+    ],);
+  }
+}
+
+class WeekPart extends HookConsumerWidget {
+  const WeekPart({super.key, required this.height});
+  final double height;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Container(
+      decoration: const BoxDecoration(
+        border: Border.fromBorderSide(
+            BorderSide(
+                color: borderColor,
+                width: 0.5
+            )
+        ),
+      ),
+    );
+  }
+}
+
+class DayPart extends HookConsumerWidget {
+  const DayPart({super.key, required this.height});
+  final double height;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Container(
+      decoration: const BoxDecoration(
+        border: Border.fromBorderSide(
+            BorderSide(
+                color: borderColor,
+                width: 0.5
+            )
+        ),
+      ),
     );
   }
 }
--- lib/f002_end_drawer.dart
+++ lib/f002_end_drawer.dart
@@ -11,58 +11,6 @@ enum EndDrawerMenuType {
   final String title;
 }
 
-class HighlightAbleButton extends StatelessWidget {
-  const HighlightAbleButton({super.key,
-    required this.title,
-    this.fontSize,
-    this.index,
-    required this.isHighlighted,
-    required this.onTapDown,
-    required this.onTap,
-    required this.onTapCancel,
-  });
-
-  final String title;
-  final double? fontSize;
-  final int? index;
-  final bool isHighlighted;
-  final void Function(int?) onTapDown;
-  final void Function(int?) onTap;
-  final void Function(int?) onTapCancel;
-
-  @override
-  Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-
-    return GestureDetector(
-      onTapDown: (TapDownDetails details) => onTapDown(index),
-      onTap: () => onTap(index),
-      onTapCancel: () => onTapCancel(index),
-      child: Container(
-        margin: const EdgeInsets.symmetric(horizontal: 16),
-        decoration: BoxDecoration(
-          color: isHighlighted
-              ? theme.colorScheme.secondaryContainer
-              : Colors.transparent,
-          borderRadius: BorderRadius.circular(16),
-        ),
-        child: ListTile(
-          title: Text(
-            title,
-            style: TextStyle(
-              color: isHighlighted
-                  ? theme.colorScheme.onSecondaryContainer
-                  : theme.colorScheme.onSurfaceVariant,
-              fontSize: fontSize,
-              fontWeight: FontWeight.w800,
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
 class EndDrawer extends StatefulWidget {
   const EndDrawer({super.key});
 
@@ -145,6 +93,58 @@ class _EndDrawerState extends State<EndDrawer> {
                 )
             )
         )
+    );
+  }
+}
+
+class HighlightAbleButton extends StatelessWidget {
+  const HighlightAbleButton({super.key,
+    required this.title,
+    this.fontSize,
+    this.index,
+    required this.isHighlighted,
+    required this.onTapDown,
+    required this.onTap,
+    required this.onTapCancel,
+  });
+
+  final String title;
+  final double? fontSize;
+  final int? index;
+  final bool isHighlighted;
+  final void Function(int?) onTapDown;
+  final void Function(int?) onTap;
+  final void Function(int?) onTapCancel;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return GestureDetector(
+      onTapDown: (TapDownDetails details) => onTapDown(index),
+      onTap: () => onTap(index),
+      onTapCancel: () => onTapCancel(index),
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 16),
+        decoration: BoxDecoration(
+          color: isHighlighted
+              ? theme.colorScheme.secondaryContainer
+              : Colors.transparent,
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: ListTile(
+          title: Text(
+            title,
+            style: TextStyle(
+              color: isHighlighted
+                  ? theme.colorScheme.onSecondaryContainer
+                  : theme.colorScheme.onSurfaceVariant,
+              fontSize: fontSize,
+              fontWeight: FontWeight.w800,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
